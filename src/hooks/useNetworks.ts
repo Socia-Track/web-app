1e0106426441a6531d5c40feb12ac54d025063f8
BaluK345 <bbalu0420@gmail.com>
2025-12-23 00:14:07 +0530

    solana dropdown added

--- src/hooks/useNetworks.ts
+++ src/hooks/useNetworks.ts
@@ -72,6 +72,13 @@ export const useNetworks = (): UseNetworksResponse => {
           chainId: 8453,
           currency: 'ETH',
           explorerUrl: 'https://basescan.org'
+        },
+        {
+          key: 'solana',
+          name: 'Solana Mainnet',
+          chainId: 101,
+          currency: 'SOL',
+          explorerUrl: 'https://solscan.io'
         }
       ]);
     } finally {
